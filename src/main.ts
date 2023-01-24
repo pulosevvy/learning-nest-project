03e6d3e5ccadcb0a976541bd119b926a8d9cfa56
Alexander <kasyanov.a@internet.ru>
2023-01-24 13:20:04 +0700

    fixSwagger

--- src/main.ts
+++ src/main.ts
@@ -5,16 +5,19 @@ import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.setGlobalPrefix("api");
-    await app.listen(3000);
 
-    const documentConfig = new DocumentBuilder()
+    const config = new DocumentBuilder()
       .setTitle('Learning NestJS Project')
       .setDescription('Документация к API')
       .setVersion('1.0.0')
+      .addTag('Docs')
       .build()
 
-    const document = SwaggerModule.createDocument(app, documentConfig);
-    SwaggerModule.setup('/docs', app, document)
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('/api/docs', app, document)
+
+
+    await app.listen(3000);
 }
 
 bootstrap();
